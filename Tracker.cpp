b611639aa1633f63b1c6db3b1ad235edc261c139
Ezio Cangialosi <eziocangialosi@gmail.com>
2023-04-01 20:55:12 +0200

    Divide mqtt response method

--- Tracker.cpp
+++ Tracker.cpp
@@ -76,16 +76,7 @@ void Tracker::whenMqttRx(String payload){
     this->usbDebug->wrt("Recived ping"); 
     this->cellular->sendMqtt("PONG");
   }else if(payload == "POS-RQ"){
-    float posLon = 0;
-    float posLat = 0;
-    if(this->positioning->getPos(&posLat, &posLon)){
-      this->usbDebug->wrt("Server Request POS");
-      this->sendPos(posLat, posLon);
-      this->prt_as_been_rq = true;
-    }else{
-      this->usbDebug->wrt("Position cannot be send...");
-      this->cellular->sendMqtt("POS-ERR");
-    }
+    this->mqtt_whenPosRq();
   }else if(payload.startsWith("STG=") && this->waitForParam){
     this->parseParamFrame(payload);
     this->paramSetted = true;
@@ -98,35 +89,9 @@ void Tracker::whenMqttRx(String payload){
   }else if(payload == "STS-ACK"){
     this->usbDebug->wrt("Server acknoweldge recived status");
   }else if(payload.startsWith("PRT=")){
-    if(payload.charAt(4) == '1' && !this->positioning->isProtectionEnable()){
-      this->positioning->enterPrtMode();
-      this->usbDebug->wrt("Server enable protection mode");
-      this->cellular->sendMqtt("PRT-ACK");
-    }else if(payload.charAt(4) == '0' && this->positioning->isProtectionEnable()){
-      this->positioning->quitPrtMode();
-      this->usbDebug->wrt("Server disable protection mode");
-      alarm_is_on = false;
-      this->cellular->sendMqtt("PRT-ACK");
-    }else{
-      this->cellular->sendMqtt("ERR");
-      this->usbDebug->wrt("Cannot edit protection mode");
-    }
+    this->mqtt_whenPrt(payload.charAt(4));
   }else if(payload == "SFZ-RQ"){
-    this->usbDebug->wrt("Server Request for safe zone position");
-    float sfzLat;
-    float sfzLon;
-    String msg;
-    if(this->positioning->getPrtPos(&sfzLat, &sfzLon)){
-      msg = "SFZ=";
-      msg += String(sfzLat,10);
-      msg += ",";
-      msg += String(sfzLon,10);
-      this->usbDebug->wrt("SafeZone position sended");
-    }else{
-      this->usbDebug->wrt("Cannot send safezone position");
-      msg = "ERR";
-    }
-    this->cellular->sendMqtt(msg);
+    this->mqtt_whenSfzRq();
   }else{
     this->cellular->sendMqtt("ERR");
     this->usbDebug->wrt("Message interpretation failed");
@@ -215,4 +180,51 @@ void Tracker::veh_charge_manager(){
       }
     }
   }
+}
+
+void Tracker::mqtt_whenPosRq(){
+  float posLon = 0;
+  float posLat = 0;
+  if(this->positioning->getPos(&posLat, &posLon)){
+    this->usbDebug->wrt("Server Request POS");
+    this->sendPos(posLat, posLon);
+    this->prt_as_been_rq = true;
+  }else{
+    this->usbDebug->wrt("Position cannot be send...");
+    this->cellular->sendMqtt("POS-ERR");
+  }
+}
+
+void Tracker::mqtt_whenPrt(char value){
+  if(value == '1' && !this->positioning->isProtectionEnable()){
+    this->positioning->enterPrtMode();
+    this->usbDebug->wrt("Server enable protection mode");
+    this->cellular->sendMqtt("PRT-ACK");
+  }else if(value == '0' && this->positioning->isProtectionEnable()){
+    this->positioning->quitPrtMode();
+    this->usbDebug->wrt("Server disable protection mode");
+    this->alarm_is_on = false;
+    this->cellular->sendMqtt("PRT-ACK");
+  }else{
+    this->cellular->sendMqtt("ERR");
+    this->usbDebug->wrt("Cannot edit protection mode");
+  }
+}
+
+void Tracker::mqtt_whenSfzRq(){
+  this->usbDebug->wrt("Server Request for safe zone position");
+  float sfzLat;
+  float sfzLon;
+  String msg;
+  if(this->positioning->getPrtPos(&sfzLat, &sfzLon)){
+    msg = "SFZ=";
+    msg += String(sfzLat,10);
+    msg += ",";
+    msg += String(sfzLon,10);
+    this->usbDebug->wrt("SafeZone position sended");
+  }else{
+    this->usbDebug->wrt("Cannot send safezone position");
+    msg = "ERR";
+  }
+  this->cellular->sendMqtt(msg);
 }
--- Tracker.h
+++ Tracker.h
@@ -17,7 +17,11 @@ class Tracker
       void sendPos(float aLat, float aLon); //Method to send the position to the Svr
       void sendSts(); //Method to send tracker status to the Srv
       void parseParamFrame(String payload); //Method to parse the recived STG frame (Acquire params from Srv)
-      void veh_charge_manager();
+      void veh_charge_manager(); //Method that manages the charge on vehicle battery
+      
+      void mqtt_whenPosRq(); //Called when a position request is recived
+      void mqtt_whenPrt(char value); //Called when the server try to change the protection mode value
+      void mqtt_whenSfzRq(); //Called when a safezone request is reviced
 
       bool alarm_is_on = false; //If the alarm is on/ringing
       bool waitForParam = false; //If we wait for server for parameters
@@ -26,14 +30,14 @@ class Tracker
       bool eco_mode = DEFAULT_ECO_MODE_STS; //If the eco mode is activated
 
       bool prt_as_been_rq = false; //If the protection mode as been requested by Srv
-      int old_veh_alim_state = LOW;
-      unsigned long veh_alim_time = 0;
-      bool isOnCharge = false;
+      int old_veh_alim_state = LOW; //Last vehicle state read on sensor pin
+      unsigned long veh_alim_time = 0; //Time since which power is detected on sensor pin
+      bool isOnCharge = false; //If the vehicle is on charge
     
       Communicator* cellular; //Pointor to Communicator instance (Srv communication)
       SerialDebug* usbDebug; //Pointor to SeriaDebug instance (Debug msg on usb port)
       Locator* positioning; //Pointor to Locator instance (GPS communication, acquire and parse)
-      LedIndicator* lightSign;
+      LedIndicator* lightSign; //Pointor to LedIndicator instance (Debug led)
   };
 
 
