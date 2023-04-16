5fdbc7a84ddf5af8585a4e0d09632e2880658d6b
Ezio Cangialosi <eziocangialosi@gmail.com>
2023-04-16 17:57:04 +0200

    Add doxygen comments on config.h

--- config.h
+++ config.h
@@ -12,7 +12,7 @@
 
 /*******************************************/
 // GPRS config
-#define GSM_BAUD 9600 //Serial baud used ot communicate with gprs module
+#define GSM_BAUD 9600 //!< Serial baud used ot communicate with gprs module
 
 // Select your modem:
 #define TINY_GSM_MODEM_SIM800
@@ -37,42 +37,42 @@
 // #define TINY_GSM_MODEM_XBEE
 // #define TINY_GSM_MODEM_SEQUANS_MONARCH
 
-#define GSM_PIN "" //SIM card pin
-const char APN[]      = "example.com"; //URL of your phone operator apn
-const char GPRS_USER[] = ""; //Your gprs user (if needed)
-const char GPRS_PSWD[] = ""; //Your gprs password (if needed)
+#define GSM_PIN "" //!< SIM card pin
+const char APN[]      = "example.com"; //!< URL of your phone operator apn
+const char GPRS_USER[] = ""; //!< Your gprs user (if needed)
+const char GPRS_PSWD[] = ""; //!< Your gprs password (if needed)
 
 
 /*******************************************/
 // MQTT details
-#define MQTT_PORT 1883 //Broker port
-const char BROKER[]    = "example.com"; //Broker URL or IP addr
-const char TOPIC_RX[]  = "topicRX"; //Rx Topic used for this tracker
-const char TOPIC_TX[]  = "topicTX"; //Tx Topic used for this tracker
-const char MQTT_USER[] = "mqtt_user"; //Mqtt user for this tracker
-const char MQTT_PSWD[] = "mqtt_pass"; //Mqtt password for this tracker
+#define MQTT_PORT 1883 //!< Broker port
+const char BROKER[]    = "example.com"; //!< Broker URL or IP addr
+const char TOPIC_RX[]  = "topicRX"; //!< Rx Topic used for this tracker
+const char TOPIC_TX[]  = "topicTX"; //!< Tx Topic used for this tracker
+const char MQTT_USER[] = "mqtt_user"; //!< Mqtt user for this tracker
+const char MQTT_PSWD[] = "mqtt_pass"; //!< Mqtt password for this tracker
 
 
 /*******************************************/
 // GPS config
-#define GPS_BAUD 9600 //Serial baud used for gps module
-#define DISTANCE_TRIG 5 //Distance from which we define that the position has changed, default 5 meters
+#define GPS_BAUD 9600 //!< Serial baud used for gps module
+#define DISTANCE_TRIG 5 //!< Distance from which we define that the position has changed, default 5 meters
 
 
 /*******************************************/
 //Tracker misc params
-#define DEFAULT_VEH_CHG_STS false //If the tracker can charge on vehicle battery by default
-#define DEFAULT_ECO_MODE_STS false //If the tracker should be in ecomode by default
-#define DEFAULT_SAFE_ZONE_DIAM 15 //Default diameter of the SafeZone, default 15
-#define DEFAULT_INTERVAL (5*60) //Default interval between 2 position refresh, default 5min
-#define DEFAULT_INTERVAL_WHEN_ALM 5 //Default interval between 2 position refresh when alarm is on, default 5sec
-#define MINIMAL_INTERVAL 30 //Minimal interval beetween 2 position refresh when alarm is off, default 30sec
-#define DELAY_CHG_VEH (5*60) //Default delay before charging on vehicle battery when is avail, default 5min
-#define VEH_ALIM_SENSOR_PIN 4 //Pin used to detect when the vehicle power is avail
-#define VEH_ALIM_RELAY_PIN 12 //Pin used to command the charge on vehicle battery
+#define DEFAULT_VEH_CHG_STS false //!< If the tracker can charge on vehicle battery by default
+#define DEFAULT_ECO_MODE_STS false //!< If the tracker should be in ecomode by default
+#define DEFAULT_SAFE_ZONE_DIAM 15 //!< Default diameter of the SafeZone, default 15
+#define DEFAULT_INTERVAL (5*60) //!< Default interval between 2 position refresh, default 5min
+#define DEFAULT_INTERVAL_WHEN_ALM 5 //!< Default interval between 2 position refresh when alarm is on, default 5sec
+#define MINIMAL_INTERVAL 30 //!< Minimal interval beetween 2 position refresh when alarm is off, default 30sec
+#define DELAY_CHG_VEH (5*60) //!< Default delay before charging on vehicle battery when is avail, default 5min
+#define VEH_ALIM_SENSOR_PIN 4 //!< Pin used to detect when the vehicle power is avail
+#define VEH_ALIM_RELAY_PIN 12 //!< Pin used to command the charge on vehicle battery
 
 /*******************************************/
 //Debug config
-#define USB_BAUD 115200 //Serial baud used to communicate with debug console
+#define USB_BAUD 115200 //!< Serial baud used to communicate with debug console
 
 #endif
